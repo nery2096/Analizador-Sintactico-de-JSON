5a6cd231849b22bf3895285c0e820422aa48b336
nery2096 <48324782+nery2096@users.noreply.github.com>
2019-12-04 17:28:14 -0300

    Add files via upload

---
+++ ansin.h
@@ -0,0 +1,62 @@
+/*** Librerias utilizadas ***/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+
+/*** Definiciones ***/
+
+#define L_CORCHETE 	      256
+#define R_CORCHETE	      257
+#define L_LLAVE 	        258
+#define R_LLAVE 	        259
+#define COMA		          260
+#define DOS_PUNTOS	      261
+#define STRING            262
+#define NUMBER  	        263
+#define PR_TRUE		        264
+#define PR_FALSE	        265
+#define PR_NULL           266
+#define TAMAUX              5
+#define TAMLEX             50
+#define TAMESP            101
+#define VACIO               1
+#define TAMCONJ            10
+#define TAMARRAY          500
+
+
+/*** Estructuras ***/
+
+typedef struct {
+	int compLex;
+        char lexema[TAMLEX];
+        int linea;
+} token;
+
+
+/*** Prototipos ***/
+void sigLex();
+void agregar_espacio();
+
+/*** Prototipos del Parser ***/
+void json(int setSync[]);
+void element();
+void array(int setSync[]);
+void arrayp(int setSync[]);
+void element_list();
+void element_listp(int setSync[]);
+void object(int setSync[]);
+void objectp(int setSync[]);
+void atribute_list();
+void atribute_listp(int setSync[]);
+void atribute();
+void atribute_name();
+void atribute_value(int setSync[]);
+void getToken();
+void check_input(int con_primero[], int con_siguiente[]);
+void match(int t);
+void setsync(int []);
+void parser();
+void scanto(int setSync1[], int setSync2[]);
